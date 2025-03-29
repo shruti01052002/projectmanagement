b314557b276a140302c914da22fe8a7ec4b5344b
shruti01052002 <shrutiyadav26072002@gmail.com>
2025-03-29 08:17:09 +0530

    Remove dataconnect folders from git tracking

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,7 +0,0 @@
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'projectmanagement1',
-  location: 'us-central1'
-};
-
--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,7 +0,0 @@
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'projectmanagement1',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
